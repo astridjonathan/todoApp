6b2910ae95a0eac56595f373ffdfd8d31379ce09
Astrid JONATHAN <astrid.jonathan@gmail.com>
2019-10-07 23:25:36 -0400

    w du 07
    ma todo app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import { Task } from '../models/task';
+import { TaskStorageService } from '../services/task-storage.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor(private taskStorage: TaskStorageService) {
+    this.taskStorage.getTasks().then(tasks => {
+      if (null !== tasks ) {
+        this.tasks = tasks ;
+      }
+    });
+  }
+
+  task: Task = new Task();
+  tasks: Task[] = [];
+  /**
+   * Déclenche l'enregistrement
+   * lors de la pression sur "Entrer"
+   * @param code
+   */
+  saveTask(code: string): void {
+    // -- Lorsque l'utilisateur appuie sur la touche "Entrer" ;
+    if (code === 'Enter') {
+
+      // -- Je pousse dans mon tableau la tâche ;
+      this.tasks.push(this.task);
+
+      // -- J'enregistre les tâches
+      this.taskStorage.saveTasks(this.tasks);
+
+      // -- Je remet à zero la tâche.
+      this.task = new Task();
+    }
+  }
+
+  deleteTask(id) {
+
+    this.tasks.splice(id, 1);
+    this.taskStorage.deleteTasks(this.tasks);
+
+  }
 
 }
